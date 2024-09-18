9744b0335a854393d2bf935aae00838893b1595a
Johnson Mwakazi <johnsonmwakazi@Johnsons-MacBook-Air.local>
2024-09-18 21:12:25 +0300

    initial setup

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+const index = () => {
+  return (
+    <View className="flex-1 bg-[#ff3] p-6 ">
+      <Text className="text-2xl">Welcome to react native!</Text>
+    </View>
+  );
+};
+
+export default index;
