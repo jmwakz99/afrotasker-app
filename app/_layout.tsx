bf204801d6fc44d0c0afb09c4dc17517b8937292
Johnson Mwakazi <johnsonmwakazi@Johnsons-MacBook-Air.local>
2024-09-20 15:52:14 +0300

    add signup form

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -5,7 +5,6 @@ import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { StatusBar } from "expo-status-bar";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
